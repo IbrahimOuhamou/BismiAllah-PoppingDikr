a47d4ede12738c45eb1d3d74e6cfef1a8eb89090
IbrahimOuhamou <ouhamouy10@gmail.com>
2023-05-14 11:28:12 +0100

    optimizing code

--- Dikr_exe/main.cpp
+++ Dikr_exe/main.cpp
@@ -3,14 +3,17 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <string>
-
+#include <vector>
 
 unsigned int cooldown = 3;
 
 // "/usr/share/fonts/truetype/ubuntu/UbuntuMono-RI.ttf"
+// "/home/ibrahimo/Downloads/Noto_Sans_Arabic/NotoSansArabic-VariableFont_wdth,wght.ttf"
 
 std::string DikrList[5] = {"Bismi Allah", "Allah Akbar", "Alhamdo li Allah", "Astaghfiro Allah", "La ilaha illa Allah"};
+std::vector<const char*> ArDikrVec = {"سبحن الله", "الله أكبر"};
 
+int chosenDikr = 0;
 
 int main(){
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
@@ -42,13 +45,19 @@ int main(){
     short int border = 5;
     SDL_Rect BismiAllahRect = SDL_Rect{border, border, window_width - (2 * border), window_height - (2 * border)};
     
-    TTF_Font * Mono = TTF_OpenFont("/usr/share/fonts/truetype/ubuntu/UbuntuMono-BI.ttf", 67);
+    TTF_Font * Mono = TTF_OpenFont("/home/ibrahimo/Downloads/Noto_Sans_Arabic/NotoSansArabic-VariableFont_wdth,wght.ttf", 67);
     SDL_Color dikr_color = SDL_Color{0, 0, 0};
-    
+
+/*    TTF_SetDirection(HB_DIRECTION_RTL);
+    TTF_SetScript(HB_SCRIPT_ARABIC);
+*/
+
     srand(time(NULL));
 
-    while(show_counter > 0){
 
+    //
+    while(show_counter > 0){
+        chosenDikr = rand() % ArDikrVec.size();
 
         // Create window with SDL_Renderer graphics context
         SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_POPUP_MENU);
@@ -60,9 +69,20 @@ int main(){
             return 0;
         }
 
-        SDL_Surface * Surface_Dikr = TTF_RenderUTF8_Solid(Mono, DikrList[rand() % 5].c_str(), dikr_color);
-        SDL_Texture * Dikr = SDL_CreateTextureFromSurface(renderer, Surface_Dikr);
+        //convert unicode to multibyte "https://stackoverflow.com/questions/73275395/how-do-i-join-up-arabic-glyphs-with-sdl-ttf-v2-20"
+/*        int wstr_len = (int)wcslen(ArDikrVec[chosenDikr]);
+        int num_chars = WideCharToMultiByte(CP_UTF8, 0, ArDikrVec[chosenDikr], wstr_len, NULL, 0, NULL, NULL);
+        char* strTo = (char*)malloc((num_chars + 1) * sizeof(char));
+        if (strTo)
+        {
+            WideCharToMultiByte(CP_UTF8, 0, ArDikrVec[chosenDikr], wstr_len, strTo, num_chars, NULL, NULL);
+            strTo[num_chars] = '\0';
+        }
+*/
+        SDL_Surface * Surface_Dikr = TTF_RenderUTF8_Blended(Mono, ArDikrVec[chosenDikr], dikr_color);
+        SDL_Texture * DikrTexture = SDL_CreateTextureFromSurface(renderer, Surface_Dikr);
         SDL_Rect Dikr_rect = SDL_Rect{border * 2, border * 2, window_width - (border * 4), window_height - (border * 4)};
+
 
         bool running = true;
 
@@ -73,27 +93,27 @@ int main(){
                     running = false;
                 }
                 if (SDL_MOUSEBUTTONDOWN == event.type){
-                    running = false;    
+                    running = false;
                 }
             }
             //Clear background
-            SDL_SetRenderDrawColor(renderer,255,255,0,0);
+            SDL_SetRenderDrawColor(renderer,0,255,0,0);
             SDL_RenderClear(renderer);
 
             //Draw inner box
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
             SDL_RenderDrawRect(renderer, &BismiAllahRect);
             SDL_RenderFillRect(renderer, &BismiAllahRect);
             
             //Draw Dikr
-            SDL_RenderCopy(renderer, Dikr, NULL, &Dikr_rect);
+            SDL_RenderCopy(renderer, DikrTexture, NULL, &Dikr_rect);
 
             SDL_RenderPresent(renderer);
-            }
+        }
         show_counter--;
 
         SDL_FreeSurface(Surface_Dikr);
-        SDL_DestroyTexture(Dikr);
+        SDL_DestroyTexture(DikrTexture);
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
 
---
+++ Dikr_exe/main1.cpp
@@ -0,0 +1,76 @@
+//بسم الله الرحمن الحيم
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+const char* Dikr_list[][3] = {"بسم الله", "سبحن الله", "الله أكبر"};
+
+int cooldown_time = 3;
+int show_counter = 2; // for debugging
+bool always_show = false; // for debugging
+int screen_width = 0, screen_heigth = 0;
+int window_width = 200, window_height = 100;
+
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
+
+void init();
+void pop_dikr();
+void clear();
+
+int main(){
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0){
+        printf("Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    if(TTF_Init() == -1){
+        printf("Error: %s\n", TTF_GetError());
+        return -1;
+    }
+
+#ifdef SDL_HINT_IME_SHOW_UI
+    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+#endif
+
+    init();
+
+    while(show_counter > 0){
+        pop_dikr();
+        
+        show_counter--;
+        
+        clear();
+
+        SDL_Delay(cooldown_time * 1000);
+    }
+
+
+    SDL_Quit();
+}
+
+void init(){
+    //get screen resolution
+    SDL_DisplayMode DM;
+    SDL_GetCurrentDisplayMode(0, &DM);
+    screen_heigth = DM.h;
+    screen_width = DM.w;
+}
+
+void pop_dikr(){
+    window = SDL_CreateWindow("Dikr", screen_width - window_width, screen_heigth * 3 / 10, window_width, window_height, SDL_WINDOW_POPUP_MENU);
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    if(renderer == nullptr){
+        SDL_Log("Error creating SDL_Renderer!");
+    }
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(2 * 1000);
+}
+
+void clear(){
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+}
